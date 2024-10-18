64eda7704b8cf57c37ecdc599b9949b972ceb460
smkankanamge <119028384+mSrishan@users.noreply.github.com>
2024-10-18 19:32:03 +0530

    initial commit

---
+++ src/main/java/com/innovateX/Bankora/BankoraApplication.java
@@ -0,0 +1,13 @@
+package com.innovateX.Bankora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankoraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/innovateX/Bankora/BankoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innovateX.Bankora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
